7e3ba91ae503f336932fe8ebd94401972373c2bf
Herb <i@4leaf.me>
2014-10-25 00:52:12 +0800

    more detail in readme

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 TVee
 ----
 
-抓取 http://www.yayaxz.com 上的资源，定时抓取并发送到 Aria2 RPC，或者生成RSS
+定时抓取下载链接并发送到 Aria2 RPC，或者生成RSS，目前只支持 http://www.yayaxz.com 上的资源，适合部署到 NAS 上自动下载剧集
 
 安装
 ====
 
 * 安装 python
 * 安装依赖 `pip install -r tvee/requirements.txt && pip install supervisor`
-* 安装前端依赖 `cd tvee && bower install`
+* 安装前端依赖 `cd tvee && bower install` 该步骤依赖比较复杂，你可以在本地安装好以后再服务到 NAS
 * 配置 `cp config.sample.conf config.conf && vi config.conf`
 * 启动 `supervisord -c production/supervisord.conf`
 * http://localhost:8000
+* 点击 设置 设定 迅雷用户名和密码、Aria2 RPC Path 和 下载目录
--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@
 import os
 
 from fabric.context_managers import cd
-from fabric.api import sudo, env
+from fabric.api import run, env
 from fabric.contrib import files
 
 env.shell = 'sh -c'
@@ -14,16 +14,17 @@ env.path = '$PATH:/opt/bin:/usr/local/bin'
 def deploy(app_dir):
     if not files.exists(app_dir):
         app_dir_dir = os.path.dirname(app_dir)
-        sudo('mkdir -p ' + app_dir_dir)
+        run('mkdir -p ' + app_dir_dir)
         with cd(app_dir_dir):
-            sudo('git clone https://github.com/exherb/tvee.git ' +
-                 os.path.basename(app_dir))
-            sudo('pip install supervisor')
+            run('git clone https://github.com/exherb/tvee.git ' +
+                os.path.basename(app_dir))
+            run('pip install supervisor')
         with cd(app_dir):
-            sudo('supervisord -c production/supervisord.conf')
+            run('cp config.sample.conf config.conf')
+            run('supervisord -c production/supervisord.conf')
     with cd(app_dir):
-        sudo('pip install -r tvee/requirements.txt')
-        sudo('git reset --hard')
-        sudo('git pull origin master')
-        sudo('supervisorctl -c production/supervisord.conf restart ' +
-             'tvee tvee_worker')
+        run('pip install -r tvee/requirements.txt')
+        run('git reset --hard')
+        run('git pull origin master')
+        run('supervisorctl -c production/supervisord.conf restart ' +
+            'tvee tvee_worker')
