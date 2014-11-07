8b3818e1332421b18436a9823feb3ba6855f383a
Herb <i@4leaf.me>
2014-11-08 00:44:28 +0800

    update deploy code

--- fabfile.py
+++ fabfile.py
@@ -12,19 +12,23 @@ env.path = '$PATH:/opt/bin:/usr/local/bin'
 
 
 def deploy(app_dir):
+    run('easy_install pip')
+    run('pip install supervisor')
     if not files.exists(app_dir):
         app_dir_dir = os.path.dirname(app_dir)
         run('mkdir -p ' + app_dir_dir)
         with cd(app_dir_dir):
             run('git clone https://github.com/exherb/tvee.git ' +
                 os.path.basename(app_dir))
-            run('pip install supervisor')
         with cd(app_dir):
             run('cp config.sample.conf config.conf')
             run('supervisord -c production/supervisord.conf')
     with cd(app_dir):
-        run('pip install -r tvee/requirements.txt')
         run('git reset --hard')
         run('git pull origin master')
-        run('supervisorctl -c production/supervisord.conf restart ' +
-            'tvee tvee_worker')
+        run('pip install -r tvee/requirements.txt')
+        if files.exists(os.path.join(app_dir, 'tmp', 'supervisord.sock')):
+            run('supervisorctl -c production/supervisord.conf restart ' +
+                'tvee tvee_worker')
+        else:
+            run('supervisord --configuration production/supervisord.conf')
